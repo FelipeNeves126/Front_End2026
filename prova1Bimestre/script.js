90830ce16c30ffa93846bb45d0e808af415f9c39
FelipeNeves126 <felipeneves126@gmail.com>
2026-04-22 20:05:46 -0300

    refazendo a ´prova

---
+++ prova1Bimestre/script.js
@@ -0,0 +1,9 @@
+function MostrarTabela(){
+    let container = document.getElementById('containerTabela');
+     
+    container.innerHTML= "";
+
+    let imagemTabela = document.createElement('img');
+
+    imagemTabela.src = ""
+}

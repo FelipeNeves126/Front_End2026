c7999b8ec292fecd026310a731a4d3b8619f00fe
FelipeNeves126 <felipeneves126@gmail.com>
2026-05-06 23:28:35 -0300

    js

--- atividade_04_05/script.js
+++ atividade_04_05/script.js
@@ -7,11 +7,27 @@ function addTask(){
     const newTask = document.createElement('li');
     newTask.innerHTML = `
     <span>${textItem}</span>
-    <button onclick="">Editar</button>
-    <button onclick="">Remover</button>
+    <button onclick="concluirItem(this)">Feito</button>
+    <button onclick="editarItem(this)">Editar</button>
+    <button onclick="removerItem(this)">Remover</button>
     `;
     taskList.appendChild(newTask);
     taskInput.value='';
   }
      
+}
+
+function concluirItem(button){
+  const taskComplete = button.parentElement;
+  taskComplete.classList.add('completed');
+}
+
+function editarItem(){
+
+}
+
+function removerItem(button){
+  const taskToRemove = button.parentElement;
+  taskList.removeChild(taskToRemove); 
+
 }
